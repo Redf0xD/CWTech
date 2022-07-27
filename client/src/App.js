281361a16e8ebf87c4ccbfb6b13e4efc6893b506
Redf0xD <ricardoprato36@gmail.com>
2022-07-27 18:55:19 -0400

    :sparkles: componente para mostrar informacion

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CssBaseline } from '@mui/material'
+import { Home } from './pages/home'
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Home />
+    </>
+  )
+}
+export default App
